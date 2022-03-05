589f879c62070ae712f34916c7dc5d6b2ee1067d
Francesca Colacrai <francescacolacrai@gmail.com>
2022-03-05 17:05:35 +0100

    Test

---
+++ OpenWeatherApp/src/test/java/it/univpm/OpenWeatherApp/OpenWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.OpenWeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
