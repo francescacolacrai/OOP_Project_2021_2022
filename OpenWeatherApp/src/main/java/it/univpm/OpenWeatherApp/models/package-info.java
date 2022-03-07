6d9d4852f1cd19f768ea23f8e03172aa82182a66
Francesca Colacrai <francescacolacrai@gmail.com>
2022-03-07 14:45:30 +0100

    Modifica Javadoc

---
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/models/package-info.java
@@ -0,0 +1,10 @@
+package it.univpm.OpenWeatherApp.models;
+
+/**
+ *  Questo package contiene tutte le classi che ci consentono di modellare gli oggetti 
+ *  richiesti per il corretto funzionamento di questa applicazione.import
+ *  
+ *  @author Francesca Colacrai
+ *  @author Djouaka Kelefack Lionel
+ *  
+ */
