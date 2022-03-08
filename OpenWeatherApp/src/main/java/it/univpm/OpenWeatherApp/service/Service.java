8555f89c830afde915a55a14c1405697662fb23c
Francesca Colacrai <francescacolacrai@gmail.com>
2022-03-08 12:18:23 +0100

    Modifica service

--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service.java
@@ -11,22 +11,24 @@ import org.json.simple.JSONArray;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/** Questa è l'interfaccia che contiene i metodi utilizzati dal controller per effettuare 
+ *  le richieste
+ *  
+ *  @author Francesca Colacrai
+ *  @author Djouaka Kelefack Lionel
+ */
 public interface Service {
 	
 	public abstract JSONObject getMeteoCitta(String citta);
 	public abstract JSONArray getPressioneCitta(String nome);
 	public abstract String SalvaDatiCitta(String nomeCitta);
 
-
-	public abstract ArrayList<JSONObject> readHistoryError(ArrayList<String> citta, int error, String value, int pressione)
-			throws EmptyStringException, CittaNotFoundException, WrongPeriodException, WrongValueException, IOException;
-	public abstract ArrayList<JSONArray> readVisibilityHistory(ArrayList<String> citta,String pressione)
-			throws EmptyStringException, CittaNotFoundException , WrongPeriodException, IOException;
-
-	JSONObject getNomeCitta();
-
-	JSONObject getNomeCitta(String nomeCitta);
-	//...continuare
+	public abstract ArrayList<JSONObject> readHistoryError
+		(ArrayList<String> citta, int error, String value, int pressione)
+		throws EmptyStringException, CittaNotFoundException, WrongPeriodException, 
+		WrongValueException, IOException;
 	
-
+	public abstract ArrayList<JSONArray> readVisibilityHistory(ArrayList<String> citta,
+			String pressione)
+			throws EmptyStringException, CittaNotFoundException , WrongPeriodException, IOException;
 }
--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceApi.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceApi.java
@@ -1,24 +1,74 @@
 package it.univpm.OpenWeatherApp.service;
 
 import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.springframework.web.client.RestTemplate;
+import it.univpm.OpenWeatherApp.models.*;
 
 import java.util.Map;
 import java.util.Objects;
 
+/** Questa classe è l'implementazione dell'interfaccia Service
+ * 
+ * @author Francesca Colacrai
+ * @author Djouaka Kelefack Lionel
+ * 
+ * @see Service
+ */
 public abstract class ServiceApi implements Service{
+	
+	/** api key indispensabile per ottenere dati da OpenWeather */
     private String API_KEY = "ce74fd08278903109816b3acfe7eb4fb";
 
+    /** Metodo che ottiene da OpenWeather i dati meteo di una città
+     *  @param citta  Città di cui si vogliono ottenere i dati meteo
+     *  @return jsonObject Un oggetto JSONObject che contiene le informazioni richieste
+     */
     public JSONObject getMeteoCitta(String citta) {
 
-        String url = "api.openweathermap.org/data/2.5/forecast?q=" + citta + "&appid="+API_KEY;
+    	/** Richiesta */
+        String url = "api.openweathermap.org/data/2.5/forecast?q=" + citta + "&appid="+API_KEY +
+        		"&units=metric&lang=it";
 
+        /** Oggetto della classe centrale all'interno del framework SpringBoot per poter 
+         *  eseguire richieste HTTP sincrone sul lato client
+         */
         RestTemplate restTemplate = new RestTemplate();
 
-        JSONObject jsonObject = new JSONObject(Objects.requireNonNull(restTemplate.getForObject(url,Map.class)));
+        JSONObject jsonObject = new JSONObject(Objects.requireNonNull(restTemplate.getForObject
+        		(url, Map.class)));
 
         return jsonObject;
-
     }
+    
+    /** Metodo che consente di ottenere i dati relativi alla pressione della città richiesta
+     * @param nome Nome della città di cui si vogliono avere i dati sulla pressione
+     * @return l'array JSON contenente i dati richiesti
+     */
+    public JSONArray getPressioneCitta(String nome) {
+    	int pressione;
+    	String data;
+    	
+    	JSONObject oggettoJSON = getMeteoCitta(nome);
+    	JSONArray contenitore = new JSONArray();
+    	JSONArray arrayMeteo = oggettoJSON.getJSONArray("list");
+    	
+    	for(int i=0; i<arrayMeteo.size(); i++) {
+    		pressione = (int)(arrayMeteo.getJSONObject(i)).get("pressure");
+    		data = (String)(arrayMeteo.getJSONObject(i)).get("dt_txt");
+    		
+    		JSONObject finale = new JSONObject();
+    		finale.put("Pressione", pressione);
+    		finale.put("Data", data);
+    		contenitore.put(finale);
+    	}
+    	return contenitore;
+    }
+    
+   /*
+    public String SalvaDatiCitta(StringNomeCitta) {
+    	return ;
+    }
+    */
 }
 
