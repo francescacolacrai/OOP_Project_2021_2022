0d4329aa351102a42bf1eaea5de196c6c0d5d54a
Francesca Colacrai <francescacolacrai@gmail.com>
2022-03-08 16:05:52 +0100

    Aggiunta metodi

--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceApi.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceApi.java
@@ -2,7 +2,7 @@ package it.univpm.OpenWeatherApp.service;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.JSONParser;
 import org.springframework.web.client.RestTemplate;
 import it.univpm.OpenWeatherApp.models.*;
 
@@ -28,8 +28,6 @@ public abstract class ServiceApi implements Service{
      *  @param citta  Città di cui si vogliono ottenere i dati meteo
      *  @return jsonObject Un oggetto JSONObject che contiene le informazioni richieste
      */
-    
-
     public JSONObject getMeteoCitta(String citta) {
 
     	/** Richiesta */
@@ -40,58 +38,62 @@ public abstract class ServiceApi implements Service{
          *  eseguire richieste HTTP sincrone sul lato client (API REST)
          */
         RestTemplate restTemplate = new RestTemplate();
-
-      // JSONObject dati = new JSONObject(restTemplate.getForObject(url, Map.class));
+        
         JSONObject oggettoJSON = new JSONObject(Objects.requireNonNull(restTemplate.getForObject
         		(url, Map.class)));
 
         return oggettoJSON;
     }
-    
+        
     /** Metodo che consente di ottenere i dati relativi alla pressione della città richiesta
      * @param nome Nome della città di cui si vogliono avere i dati sulla pressione
      * @return l'array JSON contenente i dati richiesti
      */
+    public JSONArray getPressioneCitta(String nomeCitta) {
     
-    
-    public JSONArray getPressioneCitta(String nome) {
-    	
-    	int pressione;
-    	String data;
-    	
-    	JSONObject oggettoJSON = getMeteoCitta(nome);
+    	/**
+    	JSONParser parser = new JSONParser();
+   	 	RestTemplate restTemplate2 = new RestTemplate();
+   	 	String dati = restTemplate2.getForObject(
+				"http://api.openweathermap.org/data/2.5/weather?q="+ nomeCitta +
+				"&appid="+ API_KEY +"&units=metric&lang=it", String.class);
+   	
+   	 	try {
+   	 		
+   	 		JSONArray array = (JSONArray)parser.parse(dati);
+   	 		
+   	 		
+   	 		JSONObject oggetto = (JSONObject)parser.parse(dati);
+   	 		JSONObject main = (JSONObject) oggetto.get("main");
+   	 		pressione = (int)main.get("pressure");
+   	
+   	 	} catch(ParseException e) {}
+   	 	*/
+   
+    	JSONObject oggettoJSON = getMeteoCitta(nomeCitta);
     	JSONArray contenitore = new JSONArray();
-    	JSONArray arrayMeteo = oggettoJSON.getJsonObject("list");
-    	
+    	JSONArray arrayMeteo = oggettoJSON.getJSONArray("list");
+  
     	for(int i=0; i<arrayMeteo.size(); i++) {
-    		pressione = (int)(arrayMeteo.getJsonObject(i)).get("pressure");
-    		data = (String)(arrayMeteo.getJsonObject(i)).get("dt_txt");
+    		
+    		int pressione = (arrayMeteo.getJSONObject(i)).getInt("pressure");
+    		String data = (arrayMeteo.getJSONObject(i)).getString("dt_txt");
     		
     		JSONObject finale = new JSONObject();
     		finale.put("Pressione", pressione);
     		finale.put("Data", data);
-    		contenitore.put(finale);
+    		contenitore.add(finale);
     	}
+    	
     	return contenitore;
     }
 
-    	JSONParser parser = new JSONParser();
-    	String dati = restTemplate.getForObject(url, String.class);
-    	
-    	try {
-    		JSONObject oggetto = (JSONObject)parser.parse(dati);
-    		JSONObject main = (JSONObject) oggetto.get("main");
-    		pressione = (int)main.get("pressure");
-    	
-    	} catch(ParseException e) {}
    
-    }
- 
-    
-   /*
-    public String SalvaDatiCitta(StringNomeCitta) {
+   
+    public String SalvaDatiCitta(String nomeCitta) {
+    	
     	return ;
     }
-    */
+    
 }
 
