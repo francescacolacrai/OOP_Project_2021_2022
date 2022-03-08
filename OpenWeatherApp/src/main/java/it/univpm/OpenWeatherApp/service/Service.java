216cb29cd701de47e733f92361e950b7d18ec5f5
lionel <djouakalionel4@gmail.com>
2022-03-08 11:23:47 +0100

    modifica

--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service.java
@@ -1,15 +1,10 @@
 package it.univpm.OpenWeatherApp.service;
 
-import it.univpm.OpenWeatherApp.exceptions.CittaNotFoundException;
-import it.univpm.OpenWeatherApp.exceptions.EmptyStringException;
-import it.univpm.OpenWeatherApp.exceptions.WrongPeriodException;
-import it.univpm.OpenWeatherApp.exceptions.WrongValueException;
+
 import it.univpm.OpenWeatherApp.models.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
-import java.io.IOException;
-import java.util.ArrayList;
 
 public interface Service {
 	
@@ -17,12 +12,6 @@ public interface Service {
 	public abstract JSONArray getPressioneCitta(String nome);
 	public abstract String SalvaDatiCitta(String nomeCitta);
 	public abstract Citta getCittafromApi(String citta);
-
-
-	public abstract ArrayList<JSONObject> readHistoryError(ArrayList<String> citta, int error, String value, int pressione)
-			throws EmptyStringException, CittaNotFoundException, WrongPeriodException, WrongValueException, IOException;
-	public abstract ArrayList<JSONArray> readVisibilityHistory(ArrayList<String> citta,String pressione)
-			throws EmptyStringException, CittaNotFoundException , WrongPeriodException, IOException;
 
 	JSONObject getNomeCitta();
 
