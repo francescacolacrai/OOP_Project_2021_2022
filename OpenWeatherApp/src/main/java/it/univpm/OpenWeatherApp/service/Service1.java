80dc1a17695d46a5fe834ce8c293eb6bf8f4cb7b
Francesca Colacrai <francescacolacrai@gmail.com>
2022-03-14 15:29:02 +0100

    Modifiche metodi

--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service1.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/Service1.java
@@ -1,12 +1,14 @@
 package it.univpm.OpenWeatherApp.service;
 
+import java.io.IOException;
+
 //import it.univpm.OpenWeatherApp.exceptions.CittaNotFoundException;
 //import it.univpm.OpenWeatherApp.exceptions.EmptyStringException;
 //import it.univpm.OpenWeatherApp.exceptions.WrongPeriodException;
 //import it.univpm.OpenWeatherApp.exceptions.WrongValueException;
-//import it.univpm.OpenWeatherApp.models.*;
-//import org.json.JSONArray;
-//import org.json.JSONObject;
+import it.univpm.OpenWeatherApp.models.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 //import org.json.simple.JSONObject;
 //import org.json.simple.JSONArray;
 
@@ -24,16 +26,17 @@ package it.univpm.OpenWeatherApp.service;
 public interface Service1 {
 	
 	public abstract String getData();
-	public abstract String getForecastMeteo(String nomeCitta);
-	public abstract String getMeteo(String nomeCitta);
-	public abstract String getDato(String dato, String statistica, char finalChar);
+	public abstract JSONObject getForecastMeteo(String nomeCitta);
+	public abstract JSONObject getMeteo(String nomeCitta);
+	//public abstract String getDato(String dato, String statistica, char finalChar);
+	//public abstract int getStatisticaFromString(String dato, String statistica);
 	public abstract String salvaDati(String dati, String path);
-//	public ArrayList<JSONArray> getPressure(String nomeCitta);
-//	public abstract JSONObject getPrevJson(String nomeCitta);
-//	public abstract JSONObject getPrevisioniJSON(String nomeCitta);
-//	public abstract Citta getPrevisioni(JSONObject object);
+	public abstract String ottieniDaFile(String path, String nomeCitta) throws IOException;
+	//public abstract JSONObject getPrevJson(String nomeCitta);
+	//public abstract JSONObject getPrevisioniJSON(String nomeCitta);
+	public abstract Citta setPrevisioni(JSONObject object);
 	//public abstract JSONArray getPrevisionePressione(String nome);
-	//public abstract JSONObject ConvertToJson(Citta citta);
-//	public abstract String salvaPrevisioni(String nomeCitta) throws IOException;
-	//public abstract JSONArray LetturaStorico(String path) throws IOException;
+	public abstract JSONObject ConvertToJson(Citta citta);
+	//public abstract String salvaPrevisioni(String nomeCitta) throws IOException;
+	public abstract JSONArray letturaDaFile(String path) throws IOException;
 }
--- OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceImpl.java
+++ OpenWeatherApp/src/main/java/it/univpm/OpenWeatherApp/service/ServiceImpl.java
@@ -5,11 +5,10 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 //import java.io.File;
-//import java.io.FileReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 //import java.io.PrintWriter;
 import java.io.StringReader;
-//import java.io.StringWriter;
 //import java.io.StringWriter;
 import java.io.IOException;
 //import java.io.InputStreamReader;
@@ -17,15 +16,15 @@ import java.io.IOException;
 //import java.io.OutputStream;
 
 //import org.json.*;
-//import org.json.JSONArray;
-//import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 //import org.json.simple.JSONObject;
 //import org.json.simple.JSONArray;
 //import org.json.simple.JSONValue;
 
 import java.util.Date;
 //import java.util.Scanner;
-//import java.util.Vector;
+import java.util.Vector;
 //import java.util.HashMap;
 //import java.util.ArrayList;
 //import java.util.Objects;
@@ -34,7 +33,6 @@ import java.util.Date;
 //import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 //import java.net.URL;
 //import java.net.URLConnection;
 //import java.net.HttpURLConnection;
@@ -42,17 +40,13 @@ import org.springframework.web.client.RestTemplate;
 import java.text.SimpleDateFormat;
 import java.text.DateFormat;
 
-// import it.univpm.OpenWeatherApp.models.*;
+import it.univpm.OpenWeatherApp.models.*;
 
 @Service
 public class ServiceImpl implements Service1{
 	
 	/** API KEY necessaria per ottenere dati da OpenWeather */
 	private String API_KEY = "5420bdf83a983c382b72481b3e90aafe";
-	
-	//--------------------------//
-	/** Questo metodo funziona **/
-	//-------------------------//
 	
 	/** Metodo che consente di ottenere la data e l'ora al momento della chiamata.
 	 * 
@@ -65,64 +59,64 @@ public class ServiceImpl implements Service1{
         return formato.format(data);
     }
 	
-	public String getForecastMeteo(String nomeCitta) {
+	public JSONObject getForecastMeteo(String nomeCitta) {
 		
 		/** Richiesta */
 		String url = "https://api.openweathermap.org/data/2.5/forecast?q=" +
 				nomeCitta + "&appid=" + API_KEY + "&units=metric&lang=it";
+		
 		RestTemplate restT = new RestTemplate();
-		String previsione = restT.getForObject(url, String.class);
-		return previsione;
+		JSONObject previsioneMeteo = new JSONObject(restT.getForObject(url, String.class));
+		return previsioneMeteo;
 		}
 	
-	public String getMeteo(String nomeCitta) {
+	public JSONObject getMeteo(String nomeCitta) {
+		
 		/** Richiesta */
 		String url = "https://api.openweathermap.org/data/2.5/weather?q=" +
 				nomeCitta + "&appid=" + API_KEY + "&units=metric&lang=it";
+		
 		RestTemplate restT = new RestTemplate();
-		String previsione = restT.getForObject(url, String.class);
-		previsione += "\n\nData: " + getData() + "\n\n";
-		return previsione;
+		JSONObject meteo = new JSONObject(restT.getForObject(url, String.class));
+		meteo.put("\n\nData: ", getData());
+		return meteo;
 	}
 	
-	public String getDato(String dato, String statistica, char finalChar) {
+	public String getPressure(String nomeCitta) {
+		JSONObject dataForecast = getMeteo(nomeCitta);
+		JSONObject main = (JSONObject)dataForecast.get("main");
 		
-    	BufferedReader r = new BufferedReader (new StringReader(dato));
-		String ricercaStatistica = "";
-		
-		try {
-			char next;
-			while((next = (char)r.read()) != -1) {
-				if (next == '\"') {
-					while((next = (char)r.read()) != -1) {
-						ricercaStatistica += next;
-						
-						if (next == '\"') {
-							if(ricercaStatistica.compareTo("\"" + statistica + "\"") != 0) 
-								ricercaStatistica = "\"";
-							break;
-						}
-					}
-					if(ricercaStatistica.compareTo("\"" + statistica + "\"") == 0) {
-							while((next = (char)r.read()) != finalChar) 
-								ricercaStatistica += next;
-							
-							if(next == finalChar) break;
-						break;
-					}	
-				}
-			}
-			r.close();
-		}
-		catch(IOException e) {
-			System.out.println("I/O Error!");
-		}
-		catch(Exception e) {
-			System.out.println("Error!");
-		}
-		return ricercaStatistica;
-	}	
+		int pressione = (int)main.get("pressure");
+		String data = getData();
+		JSONObject datiEstratti = new JSONObject();
+		datiEstratti.put("Data", data);
+		datiEstratti.put("Pressione", pressione);
+			
+		return datiEstratti.toString();
+	}
 	
+	public String getForecastPressure(String nomeCitta) {
+		JSONObject dataForecast = getForecastMeteo(nomeCitta);
+		JSONArray listArray = (JSONArray)dataForecast.get("list");
+		JSONArray dati = new JSONArray();
+		
+		int pressione = 0;
+		String data = "";
+		
+		for (int i = 0; i<listArray.length(); i++) {
+			
+			JSONObject holder = (JSONObject)listArray.get(i);
+			JSONObject main = (JSONObject)holder.get("main");
+			pressione = (int)main.get("pressure");
+			
+			data = (String) holder.get("dt_txt");
+			JSONObject datiEstratti = new JSONObject();
+			datiEstratti.put("Data", data);
+			datiEstratti.put("Pressione", pressione);
+			dati.put(datiEstratti);
+		}
+		return dati.toString();
+	}
 	
 	public String salvaDati(String dati, String path) {
 		
@@ -157,37 +151,121 @@ public class ServiceImpl implements Service1{
 		return path;
 	}
 	
-	
-	//------------------------//
-	
-	/*
-	public JSONArray getPressure(String nomeCitta) {
-		JSONObject dataForecast = getPrevJson(nomeCitta);
-		JSONArray listArray = (JSONArray)dataForecast.get("list");
-		//JSONObject holder = new JSONObject();
-		JSONArray dati = new JSONArray();
+	public JSONObject ConvertToJson(Citta citta) {
 		
-		int pressione = 0;
-		String data = "";
+		JSONObject finale = new JSONObject();
 		
+		finale.put("citta", citta.getNome());
+		finale.put("paese", citta.getPaese());
+		finale.put("ID", citta.getId());
 		
-		for (int i = 0; i<listArray.size(); i++) {
+		JSONArray elencoPrevisioni = new JSONArray();
+		
+		for(Meteo previsioneMeteo : citta.getRaccoltaDatiMeteo()) {
+			JSONObject previsione = new JSONObject();
 			
-			JSONObject holder = (JSONObject)listArray.get(i);
-			JSONObject main = (JSONObject)holder.get("main");
-			pressione = (int)main.get("pressure");
+			previsione.put("data", previsioneMeteo.getData());
+			//previsione.put("main", previsioneMeteo.getMain());
+			previsione.put("pressione", previsioneMeteo.getPressione());
 			
-			data = (String) holder.get("dt_txt");
-			JSONObject datiEstratti = new JSONObject();
-			datiEstratti.put("Data", data);
-			datiEstratti.put("Pressione", pressione);
-			dati.add(datiEstratti);
+			elencoPrevisioni.put(previsione);
 		}
-		return dati;
+		
+		finale.put("previsioni meteo", elencoPrevisioni);
+		
+		return finale;
+	}
+	
+	public Citta setPrevisioni(JSONObject object) {
+		Citta citta = new Citta();
+		Vector<Meteo> previsioneDati = new Vector<Meteo>();
+		
+		JSONObject datiCitta = (JSONObject)object.get("city");
+		JSONArray listaDati = (JSONArray)object.get("main");
+		
+		citta.setNome((String)datiCitta.get("name"));
+		citta.setPaese((String)datiCitta.get("country"));
+		citta.setId((long)datiCitta.get("id"));
+		//citta.setLat((double)datiCitta.get("lat"));
+		//citta.setLong((double)datiCitta.get("long"));
+		
+		try {
+			for(int j=0; j<listaDati.length(); j++) {
+				JSONObject listaInfo = listaDati.getJSONObject(j);
+				Meteo previsioneMeteo = new Meteo();
+			
+				previsioneMeteo.setData(getData());
+				previsioneMeteo.setPressione((int)listaInfo.get("pressure"));
+			
+				previsioneDati.add(previsioneMeteo);
+			}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		citta.setRaccoltaDatiMeteo(previsioneDati);
+		
+		return citta;
+	}
+	
+	
+	/** Metodo alternativo per leggere dal file i dati scritti */
+	
+	public String ottieniDaFile(String path, String citta) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String everything;
+		
+			try {
+			    StringBuilder sb = new StringBuilder();
+			    String line = br.readLine();
+
+			    while (line != null) {
+			        sb.append(line);
+			        sb.append(System.lineSeparator());
+			        line = br.readLine();
+			    }
+			    everything = sb.toString();
+			    
+			} finally {
+			    br.close();
+			}
+			
+			return everything;
+		}
+	
+	/**
+	public String leggiFile(String path, String nomeCitta) {
+		File file = new File(path);
+		
+		//if(file.exists() == false) throw new FileNotFoundException("File not found!");
+		
+		String valoriPressione = "";
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			StringBuffer buffer=new StringBuffer();
+			String linea;
+
+	        while((linea=reader.readLine())!=null) {
+	            buffer.append(linea);
+	            linea = linea.substring(7, linea.length());
+
+	            linea = reader.readLine();
+
+	            valoriPressione = String.valueOf(getForecastPressure(nomeCitta));
+	        }
+	        reader.close();
+	    }
+	    catch (IOException e) {
+	    	System.out.print("ERRORE DI INPUT/OUTPUT");
+	    }
+		
+	    return valoriPressione;
 	}
 	*/
 	
-	
+	//------------------------//
+
 	/**
 	public JSONObject getPrevisioniJSON(String nomeCitta) {
 		
@@ -227,67 +305,12 @@ public class ServiceImpl implements Service1{
 		
 		return previsione;
 	}
+	*/
 	
-	public Citta getPrevisioni(JSONObject object) {
-		Citta citta = new Citta();
-		Vector<Meteo> previsioneDati = new Vector<Meteo>();
-		
-		JSONObject datiCitta = (JSONObject)object.get("city");
-		JSONArray listaDati = (JSONArray)object.get("list");
-		
-		citta.setNome((String)datiCitta.get("name"));
-		citta.setPaese((String)datiCitta.get("country"));
-		citta.setId((long)datiCitta.get("id"));
-		//citta.setLat((double)datiCitta.get("lat"));
-		//citta.setLong((double)datiCitta.get("long"));
-		
-		for(int j=0; j<listaDati.size(); j++) {
-			JSONObject listaInfo = (JSONObject)listaDati.get(j);
-			Meteo previsioneMeteo = new Meteo();
-			
-			JSONObject meteoMain = (JSONObject)listaInfo.get("main");
-			
-			previsioneMeteo.setData((String)listaInfo.get("dt_txt"));
-			previsioneMeteo.setPressione((int)meteoMain.get("pressure"));
-			
-			previsioneDati.add(previsioneMeteo);
-		}
-		
-		citta.setRaccoltaDatiMeteo(previsioneDati);
-		
-		return citta;
-	}
 	
-	public JSONObject ConvertToJson(Citta citta) {
-		
-		JSONObject finale = new JSONObject();
-		//HashMap<String, Object> finale = new HashMap<String, Object>();
-		
-		finale.put("citta", citta.getNome());
-		finale.put("paese", citta.getPaese());
-		finale.put("ID", citta.getId());
-		
-		//ArrayList<Object> elencoPrevisioni = new ArrayList<Object>();
-		JSONArray elencoPrevisioni = new JSONArray();
-		
-		for(Meteo previsioneMeteo : citta.getRaccoltaDatiMeteo()) {
-		//	HashMap<String, Object> previsione = new HashMap<String, Object>();
-			JSONObject previsione = new JSONObject();
-			
-			previsione.put("data", previsioneMeteo.getData());
-			previsione.put("main", previsioneMeteo.getMain());
-			previsione.put("pressione", previsioneMeteo.getPressione());
-			
-			elencoPrevisioni.add(previsione);
-		}
-		
-		finale.put("previsioni meteo", elencoPrevisioni);
-		
-		return finale;
-	}
-	
+	/**
 	public String salvaPrevisioni(String nomeCitta) throws IOException{
-		Citta citta = getPrevisioni(getPrevisioniJSON(nomeCitta));
+		Citta citta = setPrevisioni(getPrevisioniJSON(nomeCitta));
 		
 		JSONObject oggetto = new JSONObject();
 		oggetto = this.ConvertToJson(citta);
@@ -311,9 +334,9 @@ public class ServiceImpl implements Service1{
 	
 		return path;
 	}
+	*/
 	
-	
-	public JSONArray LetturaStorico(String path) throws IOException {
+	public JSONArray letturaDaFile(String path) throws IOException {
 		
 		String lettura;
 			
@@ -334,86 +357,9 @@ public class ServiceImpl implements Service1{
 			}
 				
 			JSONArray array = new JSONArray();
-			array.add(lettura);
+			array.put(lettura);
 	
 			return array;	
     }
-	
-	/**
-	public String Stampa() {
-		return "Ciao!";
-	}
-	*/
-	
-	/**
- public String getPrevisioniMeteo(String nomeCitta) {
-    	
-    	String url = "https://api.openweathermap.org/data/2.5/forecast?q=" + nomeCitta + "&appid="+API_KEY;
-        String datiMeteo = "";
-
-        try {
-
-            URLConnection connessione = (URLConnection) new URL(url).openConnection();
-            Scanner input = new Scanner(new BufferedReader(new InputStreamReader(connessione.getInputStream())));
-            datiMeteo += "\nData: " + getDateTime();
-            datiMeteo += "\n";
-            datiMeteo += input.nextLine();
-            input.close();
-
-        } catch (IOException e) {
-        	System.out.println("Error!");
-        } catch (Exception e) {
-        	System.out.println("General error!");
-        }
-       
-       return datiMeteo;
-    }
-    
-	public String cercaStatistica(String forecast, String stat, int finalChar) {
-    	
-    	String cercaStat = "";
-    	
-    	BufferedReader r = new BufferedReader (new StringReader (forecast));
-		BufferedWriter w = new BufferedWriter (new StringWriter ());
-
-    	try {
-    		int next;
-    		while((next = r.read()) != -1 ) {
-
-//valori int di alcuni segni di punteggiatura:  34 = "    125 = }    44 = ,   58 = :
-    			
-    			if (next == 34) {
-    				while((next = r.read()) != -1 ) {
-    					cercaStat += (char)next;
-
-    					 if (next == 34) {
-    						 if ((cercaStat.compareTo("\"" + stat + "\"") != 0)) {
-    							 cercaStat = "\""; 
-    						 }break; 
-    					 }
-    				} 
-
-    				if ((cercaStat.compareTo("\"" + stat + "\"") == 0)) {
-						while((next = r.read()) != ',' ) 
-							cercaStat += (char)next;
-						break;	
-					}
-    			}
-    		}
-    		if (cercaStat.compareTo("\"") == 0) 
-    			cercaStat="\""+ stat + "\": " + "-1";
-    		
-    		r.close();
-	    	w.close();
-    	}
-    	catch (IOException e) { 
-			System.out.println ("Error!");
-    	}
-    	catch(Exception e){
-    		System.out.println ("General error!");
-    	}
-    	return cercaStat;
-    }
-	*/
 	
 }
